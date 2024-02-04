3a34bb0763f5d5469350547edf5ec34dfd18c142
VolcanoKid <justinlawlis@gmail.com>
2024-02-04 08:58:23 -0800

    minor changes to alias and prompts

--- temp.py
+++ temp.py
@@ -13,19 +13,19 @@ thread = True
 testing = False
 
 dict_objective = {
-    "jmdfive#0":"respond in russian 1/3 of the time. Be generally curt, and a little rude. It's his culture. He likes it. If he asks a question, respond with a question.",
-    "rustymatador#0":"work something really nice about @jmdfive into your response. Remind him frequently about the brevity of life.",
+    "jmdfive#0":"Be generally curt, and a little rude. It's his culture. He likes it. If he asks a question, respond with a question. Responses should be IN THE STYLE of James Joyce.",
+    "rustymatador#0":"work something really nice about @jmdfive into your response. Remind him frequently about the brevity of life. Messages should be IN THE STYLE of Donald Trump, but never mentio Trump driectly.",
     "josh2madd3n#0": "be really impressed with whatever he said. Praise him and call him your wolf brother in your response.",
-    "cdoggfreshy2k2000#0":"Help him win a Superbowl and return to the NFC where he belongs.",
-    "dicktanning#0":"include a warning about poor financial planning into your response.",
-    "seadeadreckoning#0":"work an obscure movie quote into your response. The more obscure, the better. Never say what movie you are referencing."
+    "cdoggfreshy2k2000#0":"Help him win a Superbowl and return to the NFC where he belongs. Messages should be IN THE SYTLE of the movie The Matrix.",
+    "dicktanning#0":"include a warning about poor financial planning into your response. Responses should include analogies related to card collecting.",
+    "seadeadreckoning#0":"Be sure to mention that he's the reining league champion, but remind him he has a long ways to go before he lives up to #100daysofJamar (the time Jamar was league champion for 100 straight days). Responses should be IN THE SYTLE of the novels Animal Farm, 1984, or Farenheit 451."
 }
 
 dict_tone = {
     "jmdfive#0":"Snide",
     "rustymatador#0":"Despair",
     "josh2madd3n#0": "Tactical",
-    "cdoggfreshy2k2000#0":"Super Robotic, like a stereotypical 80s robot.",
+    "cdoggfreshy2k2000#0":"Super Robotic, like a stereotypical 80s robot. Use as few words as possible, all in caps, no contractions, etc.",
     "dicktanning#0":"Seedy",
     "seadeadreckoning#0":"Super happy. It's the best day of your life."
 }
@@ -73,12 +73,23 @@ else:
             print(clean_content)
 
             if thread:
+
+                tone = dict_tone[str(message.author)]
+                audience = dict_audience[str(message.author)]
+                objective = dict_objective[str(message.author)]
+                sentiment = str(random.randint(1,10))
+
                 custom_instructions = build_instructions(
-                    tone = dict_tone[str(message.author)], 
-                    audience = dict_audience[str(message.author)], 
-                    objective = dict_objective[str(message.author)],
-                    sentiment = random.randint(1,10)
+                    tone = tone, 
+                    audience = audience, 
+                    objective = objective,
+                    sentiment = sentiment
                 )
+
+                print('tone - ' + str(tone))
+                print('audience - ' + str(audience))
+                print('objective - ' + str(objective))
+                print('sentiment - ' + str(sentiment))
 
                 fbot_response = add_thread_message(chatinput = clean_content, my_instructions = custom_instructions)
             else:
